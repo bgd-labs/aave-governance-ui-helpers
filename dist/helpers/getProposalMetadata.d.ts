4dedbf2458b31bb3907691779f40015aa1bcc539
Alex <argeares97@gmail.com>
2023-09-29 11:42:03 +0300

    fix: remove dist folder from repo

--- dist/helpers/getProposalMetadata.d.ts
+++
@@ -1,4 +0,0 @@
-import { ProposalMetadata } from '../types';
-export declare const ipfsGateway = "https://ipfs.io/ipfs";
-export declare function getLink(hash: string, gateway: string): string;
-export declare function getProposalMetadata(hash: string, gateway?: string, setIpfsError?: (hash: string, text?: string) => void, errorText?: string): Promise<ProposalMetadata>;
--- dist/lowdb/helpers.d.ts
+++
@@ -1,3 +0,0 @@
-import { CoreNetworkName } from '../helpers/appConfig';
-export declare const coreName: CoreNetworkName;
-export declare const baseDirName: string;
