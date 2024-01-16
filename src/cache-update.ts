e15927d658123b01cb3ac725ed0950fbca3eb331
sakulstra <lukasstrassel@googlemail.com>
2024-01-16 22:01:46 +0100

    fix: update to viem 2

--- src/cache-update.ts
+++ src/cache-update.ts
@@ -15,9 +15,9 @@ import {
 } from './events/payloadsController';
 import { getVotingMachineEvents } from './events/votingMachine';
 import {
+  AbiStateMutability,
   Address,
-  ContractFunctionResult,
-  GetContractReturnType,
+  ContractFunctionReturnType,
   getContract,
 } from 'viem';
 import {
@@ -28,8 +28,12 @@ import {
 
 async function cacheGovernance(): Promise<{
   proposalsCache: Record<
-    number,
-    ContractFunctionResult<typeof IGovernanceCore_ABI, 'getProposal'>
+    string,
+    ContractFunctionReturnType<
+      typeof IGovernanceCore_ABI,
+      AbiStateMutability,
+      'getProposal'
+    >
   >;
   eventsCache: Awaited<ReturnType<typeof getGovernanceEvents>>;
   ipfsCache: Record<string, ProposalMetadata>;
@@ -40,14 +44,24 @@ async function cacheGovernance(): Promise<{
   const contract = getContract({
     abi: IGovernanceCore_ABI,
     address: address,
-    publicClient: client,
+    client,
   });
   const proposalsCount = await contract.read.getProposalsCount();
   if (proposalsCount == BigInt(0))
     return { proposalsCache: {}, eventsCache: [], ipfsCache: {} };
   // cache data
   const proposalsPath = `${appConfig.govCoreChainId.toString()}/proposals`;
-  const proposalsCache = readJSONCache(proposalsPath, address) || {};
+  const proposalsCache =
+    readJSONCache<
+      Record<
+        string,
+        ContractFunctionReturnType<
+          typeof IGovernanceCore_ABI,
+          AbiStateMutability,
+          'getProposal'
+        >
+      >
+    >(proposalsPath, address) || {};
   const proposalsToCheck = [...Array(Number(proposalsCount)).keys()];
   for (let i = 0; i < proposalsToCheck.length; i++) {
     if (!proposalsCache[i] || !isProposalFinal(proposalsCache[i].state)) {
@@ -99,12 +113,12 @@ async function cacheGovernance(): Promise<{
 }
 
 async function cachePayloadsControllers(controllers: Map<Address, number>) {
-  return await Promise.allSettled(
+  return await Promise.all(
     Array.from(controllers).map(async ([address, chainId]) => {
       const client = CHAIN_ID_CLIENT_MAP[chainId];
       const contract = getContract({
         abi: IPayloadsControllerCore_ABI,
-        publicClient: client,
+        client,
         address,
       });
       const payloadsCount = await contract.read.getPayloadsCount();
@@ -114,8 +128,7 @@ async function cachePayloadsControllers(controllers: Map<Address, number>) {
       // cache data
       const payloadsPath = `${chainId}/payloads`;
       const payloadsCache =
-        readJSONCache(
-          /*<
+        readJSONCache<
           Record<
             number,
             ContractFunctionReturnType<
@@ -124,9 +137,7 @@ async function cachePayloadsControllers(controllers: Map<Address, number>) {
               'getPayloadById'
             >
           >
-        >*/ payloadsPath,
-          address,
-        ) || {};
+        >(payloadsPath, address) || {};
       const payloadsToCheck = [...Array(Number(payloadsCount)).keys()];
       for (let i = 0; i < payloadsToCheck.length; i++) {
         if (!payloadsCache[i] || !isPayloadFinal(payloadsCache[i].state)) {
@@ -136,17 +147,21 @@ async function cachePayloadsControllers(controllers: Map<Address, number>) {
       writeJSONCache(payloadsPath, address, payloadsCache);
       // cache events
       const eventsPath = `${chainId}/events`;
-      const eventsCache = readJSONCache(eventsPath, address) || [];
+      const eventsCache =
+        readJSONCache<Awaited<ReturnType<typeof getPayloadsControllerEvents>>>(
+          eventsPath,
+          address,
+        ) || [];
       const lastSeenBlock =
         eventsCache.length > 0
           ? BigInt(eventsCache[eventsCache.length - 1].blockNumber)
           : (
               await getBlockAtTimestamp({
                 client: client,
-                timestamp: payloadsCache[0].createdAt,
+                timestamp: BigInt(payloadsCache[0].createdAt),
                 fromBlock: BigInt(0),
                 toBlock: currentBlockOnPayloadsControllerChain,
-                maxDelta: BigInt(60 * 60 * 24),
+                maxDelta: BigInt(60 * 60), // 1h
               })
             ).number;
       const logs = await getPayloadsControllerEvents(
@@ -155,6 +170,7 @@ async function cachePayloadsControllers(controllers: Map<Address, number>) {
         BigInt(lastSeenBlock) + BigInt(1),
         currentBlockOnPayloadsControllerChain,
       );
+
       writeJSONCache(eventsPath, address, [...eventsCache, ...logs]);
     }),
   );
@@ -165,7 +181,7 @@ async function cacheVotes(votingPortals: Set<Address>) {
     Array.from(votingPortals).map(async (portal) => {
       const portalContract = getContract({
         abi: IVotingPortal_ABI,
-        publicClient: CHAIN_ID_CLIENT_MAP[appConfig.govCoreChainId],
+        client: CHAIN_ID_CLIENT_MAP[appConfig.govCoreChainId],
         address: portal,
       });
       return {
@@ -175,6 +191,8 @@ async function cacheVotes(votingPortals: Set<Address>) {
     }),
   );
 
+  console.log('machines', votingMachines);
+
   return await Promise.all(
     votingMachines.map(async ({ chainId, machine }) => {
       const path = `${chainId}/events`;
@@ -182,7 +200,11 @@ async function cacheVotes(votingPortals: Set<Address>) {
       const client = CHAIN_ID_CLIENT_MAP[chainId];
       const currentBlockOnPayloadsControllerChain =
         await client.getBlockNumber();
-      const votesCache = readJSONCache(path, address) || [];
+      const votesCache =
+        readJSONCache<Awaited<ReturnType<typeof getVotingMachineEvents>>>(
+          path,
+          address,
+        ) || [];
       const lastSeenBlock =
         votesCache.length > 0
           ? BigInt(votesCache[votesCache.length - 1].blockNumber)
@@ -215,24 +237,27 @@ async function cacheVotes(votingPortals: Set<Address>) {
 async function updateCache() {
   // cache governance logs
   const { proposalsCache } = await cacheGovernance();
+  console.log('caching governance finished');
 
   // cache payloadsController logs
   const controllers = new Map<Address, number>();
   Object.keys(proposalsCache).map((key) => {
-    const proposal = proposalsCache[key as unknown as number];
+    const proposal = proposalsCache[key];
     proposal.payloads.map((payload) =>
       controllers.set(payload.payloadsController, Number(payload.chain)),
     );
   });
   await cachePayloadsControllers(controllers);
+  console.log('caching payloadsController finished');
 
   // cache votes
   const votingPortals = new Set<Address>();
   Object.keys(proposalsCache).map((key) => {
-    const proposal = proposalsCache[key as unknown as number];
+    const proposal = proposalsCache[key];
     votingPortals.add(proposal.votingPortal);
   });
   await cacheVotes(votingPortals);
+  console.log('caching votes finished');
 }
 
 updateCache();
