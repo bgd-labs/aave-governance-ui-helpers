6f65b181415dbb11e189a22d1da0a51d02853d6b
Alex <argeares97@gmail.com>
2024-02-29 12:27:39 +0200

    fix: added dts to tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,6 +4,13 @@ const config: Options = {
   entry: ['src/abis/*.ts', 'src/utils/ether-v5/**/*.ts', 'src/index.ts'],
   sourcemap: true,
   format: ['iife', 'cjs', 'esm'],
+  dts: {
+    compilerOptions: {
+      moduleResolution: 'node',
+      allowSyntheticDefaultImports: true,
+      strict: true,
+    },
+  },
   // otherwise .env is ordered wrongly
   // https://github.com/evanw/esbuild/issues/399
   splitting: false,
